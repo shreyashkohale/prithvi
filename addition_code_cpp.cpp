3856f839dc08fa09661a8f9ddf1060ff43926ba6
shreyash kohale <kohaleshreyash@gmail.com>
2023-10-20 11:36:58 +0530

    done

---
+++ addition_code_cpp.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	float d;
+	cout<<"enter the value of a"<<endl;
+	cin>>a;
+	cout<<"enter the value of b"<<endl;
+	cin>>b;
+	c=a+b;
+	cout<<"shreyash kohale "<<endl;
+	cout<<" a and b is "<<c<<endl;
+	cout<<"addition of a and b is "<<c<<endl;
+	c=a-b;
+	cout<<"substraction of a and b is "<<c<<endl;
+	c=a*b;
+	cout<<"multiplication of a and b is "<<c<<endl;
+	d=a/b;
+	cout<<"division of a and b is "<<d;
+}
